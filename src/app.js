b87700ca12c9be7e8bea7e4b189cbbc1bb9da857
Mot0511 <suvorov.matvej9@gmail.com>
2025-11-30 18:31:10 +0300

    _

--- src/app.js
+++ src/app.js
@@ -30,10 +30,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(helmet()); // Security headers
-app.use(cors({
-  origin: ['*'],
-  credentials: true
-}));
+app.use(cors());
 // app.use(limiter);
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
